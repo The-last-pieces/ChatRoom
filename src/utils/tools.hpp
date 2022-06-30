d1d75d271bb39fc9cfb14445031908b67791d780
MnZn <2776835685@qq.com>
2022-06-30 12:46:08 +0800

    add bzero , refactor error check

--- src/utils/tools.hpp
+++ src/utils/tools.hpp
@@ -3,17 +3,36 @@
 
 #include <cstdio>
 #include <cstdlib>
+#include <cstring>
 #include <stdexcept>
 #include <errno.h>
 
+/*
+封装Linux常用的操作
+*/
+
 class ToolUtils {
+private:
+    static void throw_error(const char *msg) {
+        perror(msg);
+        throw std::runtime_error(msg);
+    }
+
 public:
-    // 检查Linux api的返回值 , 如果为-1则打印错误信息并抛出异常
-    static void check_error(int ret, const char *msg) {
-        if (ret == -1) {
-            perror(msg);
-            throw std::runtime_error(msg);
-        }
+    // 检查Linux api的返回值 , 如果是目标则error
+    static void error_when_is(int ret, const char *msg, int check = -1) {
+        if (ret == check) throw_error(msg);
+    }
+
+    // 检查Linux api的返回值 , 如果不是目标则error
+    static void error_when_not(int ret, const char *msg, int check = 0) {
+        if (ret != check) throw_error(msg);
+    }
+
+    // 填充0
+    template <typename T>
+    static void bzero(T &ptr) {
+        memset(&ptr, 0, sizeof(T));
     }
 };
 
