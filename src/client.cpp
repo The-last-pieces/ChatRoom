d1e493d353b3b0a24a191880fffcf9ac11911edc
MnZn <2776835685@qq.com>
2022-06-30 12:48:18 +0800

    add echo server base udp

--- src/client.cpp
+++ src/client.cpp
@@ -1,15 +1,15 @@
 #include "net/tcp/socket.hpp"
 #include <iostream>
 
-int main() {
-    const char *ip   = "10.0.24.2";
-    const int   port = 8889;
+const char *ip   = "10.0.24.2";
+const int   port = 8889;
 
+int tcp_main() {
     // 构造客户端socket
-    TcpSocket client(ip, port);
+    Socket client(ip, port);
 
     // 连接服务端
-    client.connect_to_server();
+    client.connect_server();
 
     printf("client<%s:%d> start\n", client.get_ip().data(), client.get_port());
 
@@ -21,9 +21,28 @@ int main() {
 
         client.send_data(buf);
 
-        buf = client.recv_data(1024);
+        client.recv_data(buf, 1024);
         printf("recv from server: '\n%s\n'\n", buf.data());
     }
 
     return 0;
+}
+
+int udp_main() {
+    Socket      client(ip, port, SocketType::UDP);
+    std::string buf;
+
+    while (true) {
+        printf("input to send: ");
+        std::getline(std::cin, buf);
+
+        client.sendto_data(buf);
+
+        int len = client.recvfrom_data(buf, 1024);
+        printf("recv %d from server: '\n%s\n'\n", len, buf.data());
+    }
+}
+
+int main() {
+    udp_main();
 }
--- src/server.cpp
+++ src/server.cpp
@@ -1,35 +1,60 @@
 #include "net/tcp/socket.hpp"
 #include <iostream>
 #include <thread>
+#include <optional>
 
-int main() {
-    const char *ip      = "10.0.24.2";
-    const int   port    = 8889;
-    const int   backlog = 5;
+const char *ip      = "10.0.24.2";
+const int   port    = 8889;
+const int   backlog = 5;
+const int   buf_len = 1024;
 
+int tcp_main() {
     // 服务端创建socket
-    TcpSocket server(ip, port);
+    Socket server(ip, port);
+    // 绑定地址信息
+    server.bind_port();
 
     // 监听端口
-    server.bind_and_listen(backlog);
+    server.listen_port(backlog);
 
     // 等待客户端连接
     while (true) {
         // 从请求队列中获取一个连接
-        TcpSocket client = server.accept_client();
+        Socket client = server.accept_client();
         std::thread([client = std::move(client)]() {
-            printf("client<%s:%d> connected\n", client.get_ip().data(), client.get_port());
+            std::string desc = client.get_desc();
+            printf("client<%s> connected\n", desc.data());
+            std::string buf;
             while (true) {
                 // 接收客户端的数据
-                std::string buf = client.recv_data(1024);
-                if (buf.empty()) {
-                    printf("client<%s:%d> quit\n", client.get_ip().data(), client.get_port());
+                int ret = client.recv_data(buf, buf_len);
+                if (ret == 0)
                     break;
-                }
-                printf("recv from client<%s:%d>: '\n%s\n'\n", client.get_ip().data(), client.get_port(), buf.data());
+                printf("recv from client<%s> :\n[%s]\n", desc.data(), buf.data());
                 client.send_data("string size : " + std::to_string(buf.size()));
             }
+            printf("client<%s> quit\n", desc.data());
         }).detach();
     }
     return 0;
+}
+
+int udp_main() {
+    Socket      server(ip, port, SocketType::UDP);
+    sockaddr_in addr;
+    std::string buf;
+
+    server.bind_port();
+
+    while (true) {
+        int ret = server.recvfrom_data(buf, 1024, addr);
+
+        printf("recv %d bytes from client<%s:%d>: '\n%s\n'\n", ret, inet_ntoa(addr.sin_addr), ntohs(addr.sin_port), buf.data());
+
+        server.sendto_data("string size : " + std::to_string(ret), addr);
+    }
+}
+
+int main() {
+    udp_main();
 }
